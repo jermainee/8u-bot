0bce02c97a5765512fd2a08bcba7eca144525f24
jermainee <34961982+jermainee@users.noreply.github.com>
2022-06-19 15:49:44 +0200

    Initial commit

---
+++ Bot.py
@@ -0,0 +1,129 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from seleniumrequests import Firefox
+import time
+
+
+class Bot:
+    def __init__(
+        self,
+        username,
+        password,
+        game_name,
+        minutes,
+        history_length,
+        initial_bet_amount,
+        loss_multiplicator,
+        low_threshold,
+        low_bet_type,
+        high_threshold,
+        high_bet_type
+    ):
+        self.game_name = game_name
+        self.minutes = minutes
+        self.history_length = history_length
+        self.initial_bet_amount = initial_bet_amount
+        self.loss_multiplicator = loss_multiplicator
+        self.low_threshold = low_threshold
+        self.low_bet_type = low_bet_type
+        self.high_threshold = high_threshold
+        self.high_bet_type = high_bet_type
+
+        webdriver = Firefox()
+        webdriver.get("https://8u.com/#/login")
+        webdriver.find_element(by=By.NAME, value="user").send_keys(username)
+        webdriver.find_element(by=By.CSS_SELECTOR, value="input[type='password']").send_keys(password)
+        webdriver.find_element(by=By.CSS_SELECTOR, value="button[type='button']").click()
+        self.webdriver = webdriver
+
+    def execute(self):
+        while True:
+            current_score = self.calculate_score()
+            print("[score]", current_score)
+
+            current_score = -10
+
+            if current_score >= self.high_threshold:
+                current_bet_type = self.high_bet_type
+            elif current_score <= self.low_threshold:
+                current_bet_type = self.low_bet_type
+            else:
+                time.sleep(self.minutes * 60)
+                continue
+
+            while not self.recursive_betting(current_bet_type, self.initial_bet_amount):
+                print("[result] loss")
+
+            print("[result] won")
+
+            time.sleep(self.minutes * 60)
+
+    def calculate_score(self):
+        response = self.webdriver.request('POST', 'https://8u.com/api/game/gameDraw/history',
+                                          data={"gameName": self.game_name, "pageNo": "1",
+                                                "pageSize": self.history_length}).json()
+
+        draw_results = response['data']['list']
+        small_draws = ["0", "1", "2", "3", "4", "5", "6", "7"]
+        score = 0
+
+        for draw_result in draw_results:
+            if draw_result['drawResult'] in small_draws:
+                score -= 1
+            else:
+                score += 1
+
+        return score
+
+    def has_won(self):
+        return False
+
+        # response = webdriver.request(
+        #   'POST',
+        #   'https://8u.com/api/game/gameOrder/list',
+        #   data={"gameName": gameName, "pageNo": "1", "pageSize": "10"}
+        # ).json()
+        # print(response)
+        # return response['data'][0]['winAllAmount'].isnumeric()
+
+    def bet(self, bet_type, bet_amount):
+        print("[bet]", bet_type, bet_amount)
+        self.webdriver.get("https://8u.com/#/gameCenter?gameName=" + self.game_name)
+
+        time.sleep(3)
+
+        if bet_type == "small":
+            WebDriverWait(self.webdriver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Small')]"))).click()
+
+        elif bet_type == "big":
+            WebDriverWait(self.webdriver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Big')]"))).click()
+
+        else:
+            print("invalid bet type")
+            return
+
+        WebDriverWait(self.webdriver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Amount of per bet']"))).send_keys(bet_amount)
+
+        WebDriverWait(self.webdriver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Bet Now')]"))).click()
+
+        # WebDriverWait(self.webdriver, 20).until(
+        #   EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'OK')]"))
+        # ).click()
+
+        # self.webdriver.execute_script("document.getElementsByName('username')[0].click();")
+
+    def recursive_betting(self, bet_type, bet_amount):
+        self.bet(bet_type, bet_amount)
+
+        print("[waiting] waiting for next game")
+        time.sleep(self.minutes * 60)
+
+        if self.has_won():
+            return True
+        else:
+            self.recursive_betting(bet_type, bet_amount * self.loss_multiplicator)
---
+++ main.py
@@ -0,0 +1,90 @@
+from Bot import Bot
+import tkinter as tk
+from tkinter import *
+
+
+def execute_bot():
+    Bot(
+        username=username.get(),
+        password=password.get(),
+        game_name=game_name.get(),
+        minutes=int(minutes.get()),
+        history_length=int(history_length.get()),
+        initial_bet_amount=float(initial_bet_amount.get()),
+        loss_multiplicator=int(loss_multiplicator.get()),
+        low_threshold=int(low_threshold.get()),
+        low_bet_type=low_bet_type.get(),
+        high_threshold=int(high_threshold.get()),
+        high_bet_type=high_bet_type.get()
+    ).execute()
+
+
+root = tk.Tk()
+canvas = tk.Canvas(root, width=600, height=300)
+
+title = Label(root, text="8u Bot").grid(row=0)
+
+# Credentials
+Label(root, text="Credentials").grid(row=1)
+
+Label(root, text="Username").grid(row=2)
+username = Entry(root)
+username.grid(row=2, column=1)
+username.insert(0, "flovo94")
+
+Label(root, text="Password").grid(row=3)
+password = Entry(root)
+password.grid(row=3, column=1)
+password.insert(0, "Aa1234567890")
+
+# Settings
+Label(root, text="Settings").grid(row=4)
+
+Label(root, text="game_name").grid(row=5)
+game_name = Entry(root)
+game_name.grid(row=5, column=1)
+game_name.insert(0, "CQK3M")
+
+Label(root, text="minutes").grid(row=6)
+minutes = Entry(root)
+minutes.grid(row=6, column=1)
+minutes.insert(0, 3)
+
+Label(root, text="history_length").grid(row=7)
+history_length = Entry(root)
+history_length.grid(row=7, column=1)
+history_length.insert(0, 10)
+
+Label(root, text="initial_bet_amount").grid(row=8)
+initial_bet_amount = Entry(root)
+initial_bet_amount.grid(row=8, column=1)
+initial_bet_amount.insert(0, 0.50)
+
+Label(root, text="loss_multiplicator").grid(row=9)
+loss_multiplicator = Entry(root)
+loss_multiplicator.grid(row=9, column=1)
+loss_multiplicator.insert(0, 3)
+
+Label(root, text="low_threshold").grid(row=10)
+low_threshold = Entry(root)
+low_threshold.grid(row=10, column=1)
+low_threshold.insert(0, -10)
+
+Label(root, text="low_bet_type").grid(row=11)
+low_bet_type = Entry(root)
+low_bet_type.grid(row=11, column=1)
+low_bet_type.insert(0, "big")
+
+Label(root, text="high_threshold").grid(row=12)
+high_threshold = Entry(root)
+high_threshold.grid(row=12, column=1)
+high_threshold.insert(0, 10)
+
+Label(root, text="high_bet_type").grid(row=12)
+high_bet_type = Entry(root)
+high_bet_type.grid(row=12, column=1)
+high_bet_type.insert(0, "small")
+
+Button(root, text='Execute', command=execute_bot).grid(row=13, column=1, sticky=W, pady=4)
+
+root.mainloop()
